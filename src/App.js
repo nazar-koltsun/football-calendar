6e91c225d9bf0a023784a3027b6513317c2f02e9
nazar_k <nazar.koltsun@plumrocket.com>
2020-06-27 21:49:09 +0300

    Created individual components to heder, main and footer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Header from "./components/Header/Header";
+import MainContent from "./components/MainContent/MainContent";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app-wrapper">
-      <header className="header">
-        Header
-      </header>
-      <main className="main-content">
-        Main Content
-      </main>
-      <footer className="footer">
-        Footer
-      </footer>
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/MainContent/MainContent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from './MainContent.module.css'
+
+function MainContent() {
+    return <main className={classes.mainContent}>Main Content</main>;
+}
+
+export default MainContent;
